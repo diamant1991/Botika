db61a8b91c8edcdecca880286eabd59dfe24f318
diamant1991 <diamant1988@mail.ru>
2015-02-09 16:09:21 +0200

    final styles

--- js/dropdown.js
+++ js/dropdown.js
@@ -4,12 +4,12 @@ $(".main-cat").click(function(){
 if ($(this).parent().hasClass("show")) {
     $("dropdown").addClass("show").children(".dropdown").hide("medium");;
     $(this).parent().toggleClass("show").children(".dropdown").slideToggle("medium");
-   
+     $(this).css("background-color","#de4e59");
     }
 
 else {
     $(this).parent().toggleClass("show").children(".dropdown").slideToggle("medium");
-   
+    $(this).css("background-color","#92a643");
     }
 });
 
@@ -19,10 +19,11 @@ $(".drop-main-cat").click(function(){
 if ($(this).parent().hasClass("show")) {
     $("dropdown-child").addClass("show").children(".dropdown-child").hide("medium");;
     $(this).parent().toggleClass("show").children(".dropdown-child").slideToggle("medium");
-
     }
+
 
 else {
     $(this).parent().toggleClass("show").children(".dropdown-child").slideToggle("medium");
     }
+   
 });
